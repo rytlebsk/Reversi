2c1757e1f19d61a7a72d62ce6e085d09b1b92fb4
rytlebsk <111889013+rytlebsk@users.noreply.github.com>
2025-03-17 12:15:50 +0800

    feat:test echo server

---
+++ Project/Backend/Backend/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <uwebsockets/App.h>
+using namespace std;
+using namespace uWS;
+
+int main() {
+	struct MyStruct
+	{
+
+	};
+
+	App().ws<string>("/*", {
+		/* WebSocket 事件處理 */
+		.open = [](auto* ws) {
+			cout << "WebSocket 連線成功!" << endl;
+		},
+		.message = [](auto* ws, string_view message, OpCode opCode) {
+			cout << message << opCode << endl;
+			ws->send(message, opCode, false);  // Echo 回傳訊息
+		},
+		.close = [](auto* ws, int /*code*/, std::string_view message) {
+			cout << "bye" << endl;
+		}
+		}).listen(9001, [](auto* token) {
+			if (token) {
+				cout << "伺服器運行在 ws://localhost:9001" << endl;
+			}
+			}).run();
+}
