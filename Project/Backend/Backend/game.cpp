a6bd2412d5da810da958e0125a82b6b4028904a7
youchentsai <youchentsai950324@gmail.com>
2025-03-20 12:02:05 +0800

    finish reversi board data
    done fill 1~4 on the reversi board
    
    //TODO:
    1.write a function to alternate main and interact when server send in position(from front end)
    2. improve algorithm of the game to make it more effection
    3.make game.cpp modularize

--- Project/Backend/Backend/game.cpp
+++ Project/Backend/Backend/game.cpp
@@ -34,9 +34,9 @@ void initialBoard(vector<vector<int>>& board) {
 }
 
 //step 2 & 3
-bool checkPiece(vector<vector<int>>& board, pair<int,int> dir, piece opponent_piece, int player) {
+bool checkPiece(vector<vector<int>>& board, pair<int, int> dir, piece opponent_piece, int player) {
 
-	pair<int,int> direction = dir;
+	pair<int, int> direction = dir;
 	int x = opponent_piece.col;
 	int y = opponent_piece.row;
 
@@ -62,7 +62,7 @@ bool checkPiece(vector<vector<int>>& board, pair<int,int> dir, piece opponent_pi
 //2.取一個Vector2f的向量(對方棋子 - 空格)
 //3.用上一步的向量取反向量的方向去找有沒有我方的棋子
 //4.將該格設為有效格子(顯示為紅色)
-void findValidSquare(vector<vector<int>>& board, vector<pair<int,int>>& validSquare, int player) {
+void findValidSquare(vector<vector<int>>& board, vector<pair<int, int>>& validSquare, int player) {
 	validSquare.clear();
 	int opponent = 3 - player;
 	vector<piece> opponentPiece;
@@ -88,7 +88,7 @@ void findValidSquare(vector<vector<int>>& board, vector<pair<int,int>>& validSqu
 				}
 				//step 2
 				if (board[row][col] == 0) {
-					pair<int,int> direction; //(x,y)
+					pair<int, int> direction; //(x,y)
 					direction.first = k;
 					direction.second = j;
 					direction.first *= -1.0f;
@@ -105,18 +105,63 @@ void findValidSquare(vector<vector<int>>& board, vector<pair<int,int>>& validSqu
 							}
 						}
 						if (!isDuplicate)
-							validSquare.push_back(pair{col,row});
+							validSquare.push_back(pair{ col,row });
 					}
 				}
 			}
 		}
 	}
+
 	for (pair<int, int> coords : validSquare) {
 		board[coords.second][coords.first] = 3; // can be placed
 	}
+
 }
 
-bool placePiece(vector<vector<int>>& board, vector<pair<int,int>>& validSquare , pair<int,int> position, int& player) {
+void findCanEatSquare(vector<vector<int>>& board, vector<pair<int, int>>& validSquare, int& player) {
+	for (int i = 0; i < validSquare.size(); i++) {
+		for (int j = -1; j <= 1; j++) {
+			for (int k = -1; k <= 1; k++) {
+				if (j == 0 && k == 0) continue;
+				int x = validSquare[i].first + k;
+				int y = validSquare[i].second + j;
+				if (x >= 8 || x < 0 || y >= 8 || y < 0) {
+					continue;
+				}
+				if (board[y][x] == 3 - player) {
+					pair<int, int> direction;
+					direction.first = k;
+					direction.second = j;
+					int _x = validSquare[i].first;
+					int _y = validSquare[i].second;
+					if (checkPiece(board, direction, piece{ y,x,3 - player }, player)) {
+						while (true) {
+							_x += direction.first;
+							_y += direction.second;
+
+							// 先檢查範圍
+							if (_x < 0 || _x >= 8 || _y < 0 || _y >= 8) {
+								break;
+							}
+
+							if (board[_y][_x] == 3 - player) {
+								board[_y][_x] = 4;  // 將board上可吃的棋子設定成4
+							}
+							else if (board[_y][_x] == player) {
+								break;  // 遇到我方棋子，停止
+							}
+							else {
+								break;  // 遇到空格，停止
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+bool placePiece(vector<vector<int>>& board, vector<pair<int, int>>& validSquare, pair<int, int> position, int& player) {
 	int row = position.second;
 	int col = position.first;
 
@@ -132,8 +177,8 @@ bool placePiece(vector<vector<int>>& board, vector<pair<int,int>>& validSquare ,
 					if (x >= 8 || x < 0 || y >= 8 || y < 0) {
 						continue;
 					}
-					if (board[y][x] == 3 - player) {
-						pair<int,int> direction;
+					if (board[y][x] == 4) {
+						pair<int, int> direction;
 						direction.first = k;
 						direction.second = j;
 						int _x = col;
@@ -147,7 +192,7 @@ bool placePiece(vector<vector<int>>& board, vector<pair<int,int>>& validSquare ,
 									break;
 								}
 
-								if (board[_y][_x] == 3 - player) {
+								if (board[_y][_x] == 4) {
 									board[_y][_x] = player;  // 翻轉
 								}
 								else if (board[_y][_x] == player) {
@@ -161,13 +206,24 @@ bool placePiece(vector<vector<int>>& board, vector<pair<int,int>>& validSquare ,
 					}
 				}
 			}
-			player = 3 - player;
 			//remove validSquare from board
 			for (int i = 0; i < board.size(); i++) {
 				for (int j = 0; j < board[0].size(); j++) {
 					board[i][j] = board[i][j] == 3 ? 0 : board[i][j];
+
 				}
 			}
+
+			//return can eat square back to opponent's piece
+			for (int i = 0; i < board.size(); i++) {
+				for (int j = 0; j < board[0].size(); j++) {
+					board[i][j] = board[i][j] == 4 ? 3 - player : board[i][j];
+				}
+			}
+			//switch player
+			player = 3 - player;
+
+
 			return true;
 		}
 	}
@@ -202,7 +258,7 @@ void checkGameOver(vector<vector<int>>& board, int whiteScore, int blackScore) {
 
 int main() {
 	vector<vector<int>> board(BOARD_SIZE, vector<int>(BOARD_SIZE, 0)); // 棋盤狀態 0:空格 1:黑 2:白
-	vector<pair<int,int>> validSquare; // 有效格子
+	vector<pair<int, int>> validSquare; // 有效格子
 	int player = 1; // 現在輪到的玩家 1:黑 2:白
 	// score
 	int whiteScore = 2;
@@ -212,6 +268,8 @@ int main() {
 	initialBoard(board); // 初始化棋盤
 
 	findValidSquare(board, validSquare, player); // 找出初始有效格子
+
+	findCanEatSquare(board, validSquare, player); // 找出初始可吃格子
 
 	while (true) {
 
@@ -237,14 +295,16 @@ int main() {
 		cin >> x >> y;
 
 		// 當有position進來後執行
-		if (placePiece(board, validSquare, {x,y}, player)) {
+		if (placePiece(board, validSquare, { x,y }, player)) {
 			calculateScore(board, whiteScore, blackScore);
 			findValidSquare(board, validSquare, player);
+			findCanEatSquare(board, validSquare, player);
 			if (validSquare.empty())
 			{
 				nonValidCount++;
 				player = 3 - player;
 				findValidSquare(board, validSquare, player);
+				findCanEatSquare(board, validSquare, player);
 				continue;
 			}
 			nonValidCount = 0;
